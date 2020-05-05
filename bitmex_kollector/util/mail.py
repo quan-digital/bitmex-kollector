7aa23dc63ca5bac71a8bf54385c48a799cae101b
canokaue <kauecano@gmail.com>
2020-05-05 18:27:47 -0300

    mail with name

--- bitmex_kollector/util/mail.py
+++ bitmex_kollector/util/mail.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     msg["From"] = "pipryno@gmail.com"
     msg["Subject"] = "Kollector Error"
     msg["To"] = settings.MAIL_TO
-    msg.set_content("Error occured, processRunner had to restart Kollector. \n \
-        Please check the following error log, master.")
+    msg.set_content("Error occured, processRunner had to restart '%s' Kollector. \n \
+        Please check the following error log, master." % settings.DATA_DIR)
     error_path = settings.DATA_DIR + '_error/error_' + str(dt.datetime.today().strftime('%Y-%m-%d')) + '.txt'
     try:
         msg.add_attachment(open(error_path, "r").read(), filename="error_file.txt")
